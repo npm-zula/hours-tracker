f110602256c3c85564d07fe3caeb43a3b92eb2b9
aftab <maftab1s@semo.edu>
2024-12-08 21:59:05 -0600

    json update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    analytics: true,
+    webAnalytics: true,
+    speedInsights: true,
+    runtime: 'nodejs18.x'
+  }),
   integrations: [tailwind()]
 });
